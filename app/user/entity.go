b8ba3efbcff376f4bc26f1c7ad0b6f0cfea53862
Aria <ariakm25@gmail.com>
2024-07-05 20:35:18 +0700

    add: auto create & update time user

--- app/user/entity.go
+++ app/user/entity.go
@@ -12,7 +12,7 @@ type User struct {
 	Name      string         `json:"name" gorm:"type:varchar(200);not null" validate:"required,min=3,max=100"`
 	Email     string         `json:"email" gorm:"type:varchar(255);not null;uniqueIndex:idx_user_email" validate:"required,email"`
 	Password  string         `json:"-" gorm:"not null" validate:"required,min=6"`
-	CreatedAt time.Time      `json:"createdAt"`
-	UpdatedAt time.Time      `json:"updatedAt"`
+	CreatedAt time.Time      `json:"createdAt" gorm:"autoCreateTime"`
+	UpdatedAt time.Time      `json:"updatedAt" gorm:"autoCreateTime;autoUpdateTime"`
 	DeletedAt gorm.DeletedAt `json:"deletedAt" gorm:"index:idx_user_deleted_at"`
 }
